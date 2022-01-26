71b275ebe0c6d3b2ac375bf1c2cd37db1b987781
Christian Kaindl <christian.kaindl@hey.com>
2022-01-26 12:50:53 +0100

    _app: Import global CSS

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import '../styles/globals.css'
 import '@christiankaindl/lyts/style.css'
+import '../styles/index.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
