24d4e32b31da4e6cf8d24960b3851d66e5ad8d87
Christian Kaindl <christian.kaindl@hey.com>
2022-01-28 11:30:25 +0100

    NavMenu: Improve menu style

--- components/NavMenu/NavMenu.css.ts
+++ components/NavMenu/NavMenu.css.ts
@@ -10,8 +10,8 @@ export const navMenu = style({
     content: '',
     display: 'block',
     position: 'absolute',
-    backgroundImage: 'linear-gradient(to bottom, rgb(255 255 255 / 0.85) 50%, transparent 100%)',
-    height: 150,
+    backgroundImage: 'linear-gradient(to bottom, rgb(255 255 255 / 0.9) 50%, transparent 100%)',
+    height: 130,
     top: 0,
     left: 0,
     right: 0,
@@ -21,9 +21,30 @@ export const navMenu = style({
 
 export const trigger = style({
   borderRadius: 9,
-  backgroundColor: mauveA.mauveA4,
+  // backgroundColor: mauveA.mauveA4,
+  backgroundColor: 'transparent',
   padding: '9px 15px',
+  marginTop: -5,
+  marginRight: -15,
   height: 45,
+  border: 'none',
+  // backdropFilter: 'blur(10px)',
+  fontSize: 15,
+  fontWeight: 500,
+  fontFamily: 'Inter',
+  // '@supports': {
+  //   'not (backdrop-filter: blur(1px))': {
+  //     backgroundColor: mauve.mauve4
+  //   }
+  // }
+})
+
+export const smallButton = style({
+  borderRadius: 99,
+  backgroundColor: mauveA.mauveA4,
+  // padding: '9px 15px',
+  height: 36,
+  width: 36,
   border: 'none',
   backdropFilter: 'blur(10px)',
   fontSize: 15,
