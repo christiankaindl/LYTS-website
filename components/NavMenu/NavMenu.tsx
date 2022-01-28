e0ccba1a1df2129afafa7daadf46f093d54e8f95
Christian Kaindl <christian.kaindl@hey.com>
2022-01-28 11:49:33 +0100

    NavMenu: Better button style

--- components/NavMenu/NavMenu.tsx
+++ components/NavMenu/NavMenu.tsx
@@ -2,7 +2,7 @@ import { FunctionComponent } from "react";
 import * as Dialog from '@radix-ui/react-dialog';
 import Sidebar from "../Sidebar/Sidebar";
 import { ChevronsUpDown, X } from "lucide-react";
-import { Row } from "@christiankaindl/lyts";
+import { Box, Row } from "@christiankaindl/lyts";
 import * as styles from './NavMenu.css'
 
 const NavMenu: FunctionComponent<{ currentTitle: string }> = function ({ currentTitle  }) {
@@ -13,14 +13,18 @@ const NavMenu: FunctionComponent<{ currentTitle: string }> = function ({ current
         <Row xAlign='space-between' asChild bleedLeft='6px' bleedRight='6px'>
           <Dialog.Trigger className={styles.trigger}>
             <span>Menu</span>
-            <ChevronsUpDown />
+            <Box xAlign='center' yAlign='center' className={styles.smallButton}>
+              <ChevronsUpDown />
+            </Box>
           </Dialog.Trigger>
         </Row>
         <Dialog.Portal>
           <Dialog.Overlay />
           <Dialog.Content style={{ position: 'fixed', top: 0, width: '100%', maxHeight: '100vh', overflow: 'auto', overscrollBehavior: 'contain' }} aria-label='LYTS main navigation'>
-            <Dialog.Close style={{ position: 'fixed', left: 'unset', right: 30, top: 30 }}>
-              <X />  
+            <Dialog.Close style={{ position: 'fixed', left: 'unset', right: 30, top: 30 }} asChild>
+              <Box xAlign='center' yAlign='center' className={styles.smallButton}>
+                <X />
+              </Box>
             </Dialog.Close>
             <Sidebar />
           </Dialog.Content>
