816c07c159b7aa6515b7b09226cfae8efdfbfdd8
Christian Kaindl <christian.kaindl@hey.com>
2022-01-30 11:24:14 +0100

    Add script to generate sidebar sections
    Scan the example and components directories and generate the sidebar items accordingly.
    
    This will simplify the Sidebar code and make new features possible, such as next/previous footer buttons on each page

---
+++ utils/buildNavigationData.js
@@ -0,0 +1,76 @@
+const fs = require('fs')
+const path = require('path')
+const matter = require('gray-matter')
+
+const componentsSortOrder = [
+  'stack',
+  'row',
+  'clamp',
+  'columns',
+  'grid',
+  'box',
+  'split',
+  'breakout'
+]
+
+
+const examples = fs
+  .readdirSync(path.join(process.cwd(), './docs/examples'))
+  .filter((name) => name !== '.DS_Store' && name !== 'template')
+  .map((name) => {
+    // Extract metadata using gray-matter
+    const data = matter(fs.readFileSync(path.join(process.cwd(), './docs/examples', `${name}/Component.mdx`)))
+
+    return {
+      id: name,
+      ...data?.data
+    }
+  })
+
+const components = fs
+  .readdirSync(path.join(process.cwd(), './docs/components'))
+  .map((name) => {
+    // Extract metadata using gray-matter
+    const data = matter(fs.readFileSync(path.join(process.cwd(), './docs/components', `${name}/${name}.mdx`)))
+
+    return {
+      id: name,
+      ...data?.data
+    }
+  })
+  .sort(function(a, b){  
+    return componentsSortOrder.indexOf(a.id) - componentsSortOrder.indexOf(b.id);
+  })
+
+const topLevel = fs
+  .readdirSync(path.join(process.cwd(), './pages'))
+  .filter((pageName) => pageName.endsWith('.mdx'))
+  .map((name) => {
+    // Extract metadata using gray-matter
+    const data = matter(fs.readFileSync(path.join(process.cwd(), './pages', `${name}`)))
+
+    return {
+      id: name.split('.')[0],
+      ...data?.data
+    }
+  })
+  .filter(({ draft }) => draft !== true)
+
+fs.writeFileSync(path.join(process.cwd(), './navigation-data.json'), JSON.stringify({
+  sections: [
+    // ...topLevel,
+    {
+      content: topLevel
+    },
+    {
+      title: 'Components',
+      id: 'components',
+      content: components
+    },
+    {
+      title: 'Examples',
+      id: 'examples',
+      content: examples
+    }
+  ]
+}, null, 2))
