bb07acd9a7b805d61196384970f67cd0d8c9ad1e
Christian Kaindl <christian.kaindl@hey.com>
2022-02-01 13:31:34 +0100

    og-images page: Add "noindex" meta

--- pages/og-images/index.tsx
+++ pages/og-images/index.tsx
@@ -3,6 +3,7 @@ import Logo from "@/components/Logo/Logo";
 import { Clamp, Row, Stack } from "@christiankaindl/lyts";
 import { blue } from "@radix-ui/colors";
 import { GetServerSideProps } from "next";
+import Head from "next/head";
 import { icons } from "pages";
 import { FunctionComponent } from "react";
 import ReactMarkdown from 'react-markdown'
@@ -33,62 +34,66 @@ const OgImage: FunctionComponent<Props> = function ({
   const Icon = iconMappings?.[title?.toLowerCase()]
 
   return (
-    // TODO: Set robot='noindex
-    <Clamp
-      clamp='900px'
-      yAlign='center'
-      style={{
-        backgroundColor: blue.blue9,
-        height: '100%',
-        width: '100%',
-        margin: 0,
-        fontFamily: 'Inter, sans-serif',
-        fontSize: '2.5em',
-        // padding: '2em 3em',
-        color: 'white',
-        boxSizing: 'border-box'
-      }}
-    >
-      <Stack gap={home ? 1.65 : 0.8} xAlign={home ? 'center' : undefined} style={{ textAlign: home ? 'center' : undefined }}>
-        <Logo size={home ? 'xlarge' : 'large'} accentColor='#0090ff' />
-        {!home && (
-          <Row gap={2}>
-            <h1 style={{ fontSize: '3em' }}>{title}</h1>
-            {Icon && <div style={{ transform: 'scale(2)' }}><Icon /></div>}
-          </Row>
-        )}
-        {home && (
-          <Row gap={0.5} style={{ transform: 'scale(2)' }}>
-            {icons.map(({ icon }) => {
-              return icon
-            })}
-          </Row>
-        )}
-        {description && (
-          <ReactMarkdown
-            components={{
-              p: ({ children }) => (
-                <p
-                  style={{
-                    fontSize: home ? '1.7em' : '1.2em',
-                    color: 'white',
-                    opacity: 0.8,
-                    WebkitLineClamp: 2,
-                    WebkitBoxOrient: 'vertical',
-                    overflow: 'hidden',
-                    display: '-webkit-box'
-                  }}
-                >
-                  {children}
-                </p>
-              )
-            }}
-          >
-            {description}
-          </ReactMarkdown>
-        )}
-      </Stack>
-    </Clamp>
+    <>
+      <Head>
+        <meta name="robots" content="noindex" />
+      </Head>
+      <Clamp
+        clamp='900px'
+        yAlign='center'
+        style={{
+          backgroundColor: blue.blue9,
+          height: '100%',
+          width: '100%',
+          margin: 0,
+          fontFamily: 'Inter, sans-serif',
+          fontSize: '2.5em',
+          // padding: '2em 3em',
+          color: 'white',
+          boxSizing: 'border-box'
+        }}
+      >
+        <Stack gap={home ? 1.65 : 0.8} xAlign={home ? 'center' : undefined} style={{ textAlign: home ? 'center' : undefined }}>
+          <Logo size={home ? 'xlarge' : 'large'} accentColor='#0090ff' />
+          {!home && (
+            <Row gap={2}>
+              <h1 style={{ fontSize: '3em' }}>{title}</h1>
+              {Icon && <div style={{ transform: 'scale(2)' }}><Icon /></div>}
+            </Row>
+          )}
+          {home && (
+            <Row gap={0.5} style={{ transform: 'scale(2)' }}>
+              {icons.map(({ icon }) => {
+                return icon
+              })}
+            </Row>
+          )}
+          {description && (
+            <ReactMarkdown
+              components={{
+                p: ({ children }) => (
+                  <p
+                    style={{
+                      fontSize: home ? '1.7em' : '1.2em',
+                      color: 'white',
+                      opacity: 0.8,
+                      WebkitLineClamp: 2,
+                      WebkitBoxOrient: 'vertical',
+                      overflow: 'hidden',
+                      display: '-webkit-box'
+                    }}
+                  >
+                    {children}
+                  </p>
+                )
+              }}
+            >
+              {description}
+            </ReactMarkdown>
+          )}
+        </Stack>
+      </Clamp>
+    </>
   )
 }
 
