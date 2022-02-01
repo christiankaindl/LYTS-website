abca01cad375c66f504aaa666480ee2ddf90bd06
Christian Kaindl <christian.kaindl@hey.com>
2022-02-01 16:53:50 +0100

    Add favicon
    Courtesy of https://realfavicongenerator.net

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,6 +6,14 @@ export default class MyDocument extends Document {
     return (
       <Html lang='en'>
         <Head>
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#0090ff" />
+          <meta name="msapplication-TileColor" content="#0090ff" />
+          <meta name="theme-color" content="#0090ff" />
+
           <style
             type="text/css"
             dangerouslySetInnerHTML={{ __html: mediaStyles }}
