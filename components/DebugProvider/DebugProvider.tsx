007001a58f87ee4d92fc8573f3e28cb487df2c22
Christian Kaindl <christian.kaindl@hey.com>
2022-02-02 23:06:07 +0100

    DebugProvider: Fix class names in during local development
    The generated vanilla-extract classNames are prefixed with debug string in local development when using Next.js.
    
    This is not the case when using the bundler is mdx-bundler, so the generated classNames don't match up and no styles are applied. Work around this by special casing the classNames for now.

--- components/DebugProvider/DebugProvider.tsx
+++ components/DebugProvider/DebugProvider.tsx
@@ -1,12 +1,14 @@
 import { FunctionComponent } from "react";
 import { debug } from '../../styles/index.css'
 
+const debugClass = !debug.startsWith('styles_debug_') ? `styles_debug_${debug}` : debug
+
 /**
  * Render all children LYTS components with extra visual cues
  */
 const DebugProvider: FunctionComponent<{ className?: string } & React.HTMLAttributes<HTMLElement>> = function DebugProvider ({ children, className, ...props }) {
   return (
-    <div {...props} className={`${debug} ${className ?? ''}`}>
+    <div {...props} className={`${debugClass} ${className ?? ''}`}>
       {children}
     </div>
   )
